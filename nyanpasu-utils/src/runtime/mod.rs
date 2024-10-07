c514dbe88b7a0a2203b17e807fa08ece422540a3
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-10-07 14:10:07 +0800

    feat(runtime): add util to run a task on current thread

--- nyanpasu-utils/src/runtime/mod.rs
+++ nyanpasu-utils/src/runtime/mod.rs
@@ -2,37 +2,55 @@ use std::{future::Future, sync::OnceLock};
 
 use tokio::runtime::Handle;
 use tokio::runtime::Runtime;
+use tokio::task::JoinHandle;
 pub static RUNTIME: OnceLock<Runtime> = OnceLock::new();
 
 pub fn default_runtime() -> Runtime {
     Runtime::new().unwrap()
 }
 
-/// Runs a future to completion on runtime.
-pub fn block_on<F: Future>(task: F) -> F::Output {
-    // prefer current
+pub fn get_runtime_handle() -> Handle {
     match Handle::try_current() {
-        Ok(handle) => handle.block_on(task),
+        Ok(handle) => handle,
         Err(_) => {
             let runtime = RUNTIME.get_or_init(default_runtime);
-            runtime.block_on(task)
+            runtime.handle().clone()
         }
     }
 }
 
-pub fn spawn<F>(task: F)
+/// Runs a future to completion on runtime.
+pub fn block_on<F: Future>(task: F) -> F::Output {
+    let handle = get_runtime_handle();
+    handle.block_on(task)
+}
+
+pub fn spawn<F>(task: F) -> JoinHandle<F::Output>
 where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
-    // prefer current runtime
-    match Handle::try_current() {
-        Ok(handle) => {
-            handle.spawn(task);
-        }
-        Err(_) => {
-            let runtime = RUNTIME.get_or_init(default_runtime);
-            runtime.spawn(task);
-        }
-    }
+    let handle = get_runtime_handle();
+    handle.spawn(task)
+}
+
+/// spawn a task in current thread, it not require the task to be Send.
+pub async fn spawn_current_thread<F: Future + 'static>(task: F) -> JoinHandle<F::Output> {
+    let local = tokio::task::LocalSet::new();
+    local.spawn_local(task)
+}
+
+/// block run a async task in current thread, it not require the task to be Send.
+pub fn block_on_current_thread<F: Future>(task: F) -> F::Output {
+    let handle = tokio::runtime::Handle::current();
+    handle.block_on(async move {
+        let local = tokio::task::LocalSet::new();
+        local.run_until(task).await
+    })
+}
+
+/// run a async task in current thread, it not require the task to be Send.
+pub async fn run_until<F: Future>(task: F) -> F::Output {
+    let local = tokio::task::LocalSet::new();
+    local.run_until(task).await
 }
