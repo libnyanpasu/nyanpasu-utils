b56c497f10ee3cefbb76cd55dea199b9751c560f
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-10-07 13:55:58 +0800

    refactor(runtime): perfer current runtime

--- nyanpasu-utils/src/runtime/mod.rs
+++ nyanpasu-utils/src/runtime/mod.rs
@@ -1,7 +1,7 @@
 use std::{future::Future, sync::OnceLock};
 
+use tokio::runtime::Handle;
 use tokio::runtime::Runtime;
-
 pub static RUNTIME: OnceLock<Runtime> = OnceLock::new();
 
 pub fn default_runtime() -> Runtime {
@@ -10,8 +10,14 @@ pub fn default_runtime() -> Runtime {
 
 /// Runs a future to completion on runtime.
 pub fn block_on<F: Future>(task: F) -> F::Output {
-    let runtime = RUNTIME.get_or_init(default_runtime);
-    runtime.block_on(task)
+    // prefer current
+    match Handle::try_current() {
+        Ok(handle) => handle.block_on(task),
+        Err(_) => {
+            let runtime = RUNTIME.get_or_init(default_runtime);
+            runtime.block_on(task)
+        }
+    }
 }
 
 pub fn spawn<F>(task: F)
@@ -19,6 +25,14 @@ where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
-    let runtime = RUNTIME.get_or_init(default_runtime);
-    runtime.spawn(task);
+    // prefer current runtime
+    match Handle::try_current() {
+        Ok(handle) => {
+            handle.spawn(task);
+        }
+        Err(_) => {
+            let runtime = RUNTIME.get_or_init(default_runtime);
+            runtime.spawn(task);
+        }
+    }
 }
