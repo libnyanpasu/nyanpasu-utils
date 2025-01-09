08222a48eaa23d62fbfafa1e1b0eb725a8fc4aa7
Petard Jonson <i@a632079.me>
2025-01-09 20:23:40 +0800

    feat: support get macos dns

--- network-utils/src/lib.rs
+++ network-utils/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(target_os = "macos")]
 pub mod macos {
+    use std::net::IpAddr;
     pub fn get_default_network_hardware_port() -> std::io::Result<String> {
         let dir = tempfile::tempdir()
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
@@ -41,9 +42,42 @@ pub mod macos {
         Ok(())
     }
 
+    pub fn get_dns(service_name: &str) -> std::io::Result<Option<Vec<IpAddr>>> {
+        let dir = tempfile::tempdir()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+        let path = dir.path().join("get-macos-dns.sh");
+        std::fs::write(
+            &path,
+            include_str!("./scripts/get-macos-dns.sh").replace("$1", service_name),
+        )?;
+        let output = std::process::Command::new("osascript")
+            .arg("-e")
+            .arg(format!(
+                "do shell script \"bash {}\"",
+                path.to_string_lossy()
+            ))
+            .output()?;
+        let output = String::from_utf8(output.stdout)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+        log::debug!("{:?}", output);
+        let dns = output
+            .split(&['\n', ' '])
+            .filter_map(|v| {
+                let ip: IpAddr = v.trim().parse().ok()?;
+                Some(ip)
+            })
+            .collect::<Vec<_>>();
+        if dns.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(dns))
+        }
+    }
+
     #[cfg(test)]
     mod tests {
         use super::*;
+        use test_log::test;
 
         #[test]
         fn test_get_default_network_hardware_port() {
@@ -55,6 +89,12 @@ pub mod macos {
         fn test_set_dns() {
             set_dns("Wi-Fi", Some("114.114.114.114")).unwrap();
             set_dns("Wi-Fi", None).unwrap();
+        }
+
+        #[test]
+        fn test_get_dns() {
+            let result = get_dns("Wi-Fi").unwrap();
+            println!("{:?}", result);
         }
     }
 }
